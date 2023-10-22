070983debdd18b0e5be15cd0195928446266d2c4
ahsanzubai1 <ahsanzubairzubair@gmail.com>
2023-10-22 16:42:11 +0500

    after reset soft

--- first.js
+++ first.js
@@ -1,2 +1,3 @@
 const gitToken = "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";
 console.log("check out ");
+consol.log("got  no conflits")
--- firstfrommain.js
+++ firstfrommain.js
@@ -1,2 +1,3 @@
 console.log('add kuch b ni ')
 console.log('from main not from from/mian ')
+console.log("Starting in main thread")
